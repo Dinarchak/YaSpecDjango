80997d9f8a73b80a781373f5b36a784e4b87920d
Dinar Fazliev <jjustdinar@gmail.com>
2023-10-05 23:35:56 +0300

    removed password

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -15,12 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*r^w1zmqfcc0nt=k958zz-6s5wt+((dwi645m9skgck9pa16+2'
+SECRET_KEY = 'secret key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
